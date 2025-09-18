f268400116a540f76b8f6367444ccc6a51a4786c
VijayaC1 <vijayadurga.chintha@nxzen.com>
2025-09-18 13:38:03 +0530

    Updated Attendance Routes

--- attendance_routes.py
+++ attendance_routes.py
@@ -100,14 +100,14 @@ async def get_monthly_summary(
         raise HTTPException(status_code=500, detail=str(e))
     
 
-@router.get("/attendance/summary-all")
-async def get_all_employees_summary(
-    month: int = None, 
+@router.get("/attendance/hr-assigned")
+async def get_assigned_hr_employees_summary(
+    month: int = None,
     year: int = None,
-    session: Session = Depends(get_session)
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
 ):
     try:
-        #determine date range
         if month and year:
             month_start = date(year, month, 1)
             if month == 12:
@@ -115,34 +115,112 @@ async def get_all_employees_summary(
             else:
                 month_end = date(year, month + 1, 1)
         else:
-            month_start = date(1900, 1, 1)  #far past
+            month_start = date(1900, 1, 1)
             month_end = datetime.now().date() + timedelta(days=1)
 
         query = text("""
-            SELECT 
-                u.id AS employee_id,
-                u.name,
-                u.email,
-                COUNT(*) FILTER (WHERE a.action='Present') AS present,
-                COUNT(*) FILTER (WHERE a.action='WFH') AS wfh,
-                COUNT(*) FILTER (WHERE a.action='Leave') AS leave
-            FROM employees u
-            LEFT JOIN attendance a 
-                ON u.id = a.employee_id 
-                AND a.date >= :month_start 
-                AND a.date < :month_end
-            GROUP BY u.id, u.name, u.email
-            ORDER BY u.name
-        """)
+                    SELECT 
+                        u.id AS employee_id,
+                        u.name,
+                        u.email,
+                    COUNT(*) FILTER (WHERE a.action='Present') AS present,
+                    COUNT(*) FILTER (WHERE a.action='WFH') AS wfh,
+                    COUNT(*) FILTER (WHERE a.action='Leave') AS leave
+                    FROM employees u
+                    JOIN employee_master em
+                        ON em.emp_id = u.id
+                        AND (:hr_id = em.hr1_id OR :hr_id = em.hr2_id)
+                    LEFT JOIN attendance a 
+                        ON u.id = a.employee_id
+                        AND a.date >= :month_start 
+                        AND a.date < :month_end
+                    GROUP BY u.id, u.name, u.email
+                    ORDER BY u.name
+                """)
 
-        result = session.execute(query, {"month_start": month_start, "month_end": month_end}).all()
+        result = session.execute(
+            query,
+            {
+                "month_start": month_start,
+                "month_end": month_end,
+                "hr_id": current_user.id
+            }
+        ).all()
+
 
         employees_summary = [
             {
                 "employee_id": r.employee_id,
                 "name": r.name,
                 "email": r.email,
-                #"department": r.department,  
+                # "department": r.department,
+                "present": r.present or 0,
+                "wfh": r.wfh or 0,
+                "leave": r.leave or 0
+            }
+            for r in result
+        ]
+
+        return employees_summary
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.get("/attendance/mgr-assigned")
+async def get_assigned_mgr_employees_summary(
+    month: int = None,
+    year: int = None,
+    session: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    try:
+        if month and year:
+            month_start = date(year, month, 1)
+            if month == 12:
+                month_end = date(year + 1, 1, 1)
+            else:
+                month_end = date(year, month + 1, 1)
+        else:
+            month_start = date(1900, 1, 1)
+            month_end = datetime.now().date() + timedelta(days=1)
+
+        query = text("""
+                    SELECT 
+                        u.id AS employee_id,
+                        u.name,
+                        u.email,
+                    COUNT(*) FILTER (WHERE a.action='Present') AS present,
+                    COUNT(*) FILTER (WHERE a.action='WFH') AS wfh,
+                    COUNT(*) FILTER (WHERE a.action='Leave') AS leave
+                    FROM employees u
+                    JOIN employee_master em
+                        ON em.emp_id = u.id
+                        AND (:mgr_id = em.manager1_id OR :mgr_id = em.manager2_id OR :mgr_id = em.manager3_id)
+                    LEFT JOIN attendance a 
+                        ON u.id = a.employee_id
+                        AND a.date >= :month_start 
+                        AND a.date < :month_end
+                    GROUP BY u.id, u.name, u.email
+                    ORDER BY u.name
+                """)
+
+        result = session.execute(
+            query,
+            {
+                "month_start": month_start,
+                "month_end": month_end,
+                "mgr_id": current_user.id
+            }
+        ).all()
+
+
+        employees_summary = [
+            {
+                "employee_id": r.employee_id,
+                "name": r.name,
+                "email": r.email,
+                # "department": r.department,
                 "present": r.present or 0,
                 "wfh": r.wfh or 0,
                 "leave": r.leave or 0
